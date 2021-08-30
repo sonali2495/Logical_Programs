532491cccce69e55366643c8e09b580d1b9ec319
sonaligadge24 <sonaligadge24@gmail.com>
2021-08-30 17:02:57 +0530

    Updated code for perfect number

---
+++ src/main/java/com/bridgelabz/Logical_Programs/PerfectNumber.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.Logical_Programs;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+	static double i, sum = 0;
+
+	private static double checkNumber(double number) {
+		double num = number;
+		for (i = 1; i <= number / 2; i++) {
+			if (num % i == 0) {
+				sum = sum + i;
+			}
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number to check if it is perfect number or not: ");
+		Scanner sc = new Scanner(System.in);
+		double number = sc.nextDouble();
+		sum = checkNumber(number);
+		if (sum == number)
+			System.out.println(number + " is a perfect number");
+		else
+			System.out.println(number + " is a not perfect number");
+
+	}
+
+}
